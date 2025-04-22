753bb9fe42ee9bd43fe9daa96f807a5e0f79b973
pitang.hilderney <hilderney.zapf@pitang.com>
2025-04-22 12:10:33 -0300

    adicionando IONIC

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zapfcorp.mymovielist',
+  appName: 'mymovielist',
+  webDir: 'dist/mymovielist/browser'
+};
+
+export default config;
